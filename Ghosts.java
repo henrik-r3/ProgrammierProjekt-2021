f41dcf60d9c223407ff6803234f8a98588b3bfe9
henrik-r3 <79112168+henrik-r3@users.noreply.github.com>
2021-02-26 09:24:57 +0100

    Create Ghosts.java

---
+++ Ghosts.java
@@ -0,0 +1,15 @@
+import javax.swing.ImageIcon;
+
+public class Ghosts{
+
+    public ghostOptics(){
+        Image green, blue, yellow, red;
+        
+        green = new ImageIcon("*/Bilder/Ghost.gif").getImage();
+        blue = new ImageIcon("*/Bilder/Ghost.gif").getImage();
+        yellow = new ImageIcon("*/Bilder/Ghost.gif").getImage();
+        red = new ImageIcon("*/Bilder/Ghost.gif").getImage();
+    }
+
+    
+}
