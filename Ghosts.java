4f6de8105a14cd427000b5b8b3643adcf193aacd
henrik-r3 <79112168+henrik-r3@users.noreply.github.com>
2021-02-26 10:48:30 +0100

    Update Ghosts.java

--- Ghosts.java
+++ Ghosts.java
@@ -1,15 +1,53 @@
 import javax.swing.ImageIcon;
 
+import jdk.tools.jmod.Main;
+
 public class Ghosts{
+     int followrange = Main.followrange;  //-> muss in main noch deklariert werden
 
     public ghostOptics(){
         Image green, blue, yellow, red;
         
-        green = new ImageIcon("*/Bilder/Ghost.gif").getImage();
-        blue = new ImageIcon("*/Bilder/Ghost.gif").getImage();
+      //green = new ImageIcon("*/Bilder/Ghost.gif").getImage();      -> Farben müssen noch ertsllt werden
+      //blue = new ImageIcon("*/Bilder/Ghost.gif").getImage();
         yellow = new ImageIcon("*/Bilder/Ghost.gif").getImage();
-        red = new ImageIcon("*/Bilder/Ghost.gif").getImage();
+      //red = new ImageIcon("*/Bilder/Ghost.gif").getImage();
     }
 
-    
+
+
+    public void greenGhostMovement(){
+        if(calculateDistance() < followrange ){
+            huntPacman();
+        } else {
+            
+        }
+    }
+    public void blueGhostMovement(){
+        if(calculateDistance() < followrange ){
+            huntPacman();
+        }
+
+    }
+    public void yellowGhostMovement(){
+        if(calculateDistance() < followrange ){
+            huntPacman();
+        }
+
+    }
+    public void redGhostMovement(){
+        if(calculateDistance() < followrange ){
+            huntPacman();
+        }
+
+    }
+
+    public void huntPacman(){
+
+    }
+
+    public int calculateDistance(){
+
+    }
+
 }
