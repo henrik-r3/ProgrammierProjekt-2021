f8a90719c9cf9c6ee55825c403668fcb5a0dca62
henrik-r3 <79112168+henrik-r3@users.noreply.github.com>
2021-02-26 08:24:31 +0100

    Update Pacman.java

---
+++ Pacman.java
@@ -0,0 +1,47 @@
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
+
+
+
+public class Pacman implements ActionListener{
+
+    boolean playing = true;
+
+    public pacmanOptics(){
+        //Pacman hat 4 Bilder für jede mögliche Richtung
+        Image up, down, left, right;
+
+        //Hier werden die 4 Bilder geladen
+        up = new ImageIcon("*/Bilder/PacUp.gif").getImage();
+        down = new ImageIcon("*/Bilder/PacDown.gif").getImage();
+        left = new ImageIcon("*/Bilder/PacLeft.gif").getImage();
+        right = new ImageIcon("*/Bilder/PacRight.gif").getImage();
+    }
+
+    public class PacmanMovement extends KeyAdapter{
+        //Wenn eine Taste gedrückt wird...
+        public void keypressed(KeyEvent e){
+            //... wird geguckt, welche Taste dies war...
+            int key = e.getKeyCode();
+            //....und wenn man spielt...    
+            if(playing){
+                //...bei passenden Tasten eine Aktion vorgenommen.
+                if(key == KeyEvent.VK_UP){
+
+                }else if(key == KeyEvent.VK_DOWN){
+
+                }else if(key == KeyEvent.VK_LEFT){
+
+                }else if(key == KeyEvent.VK_RIGHT){
+
+                }else if(key == KeyEvent.VK_ESCAPE){
+
+                }
+
+
+            }
+
+        }
+    }
+
+}
