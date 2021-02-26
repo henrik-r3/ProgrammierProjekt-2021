c87fd00692ec35858cac3a8998fbd0badeac43dc
henrik-r3 <79112168+henrik-r3@users.noreply.github.com>
2021-02-26 20:12:14 +0100

    Update 1 to Ghosts

--- Ghosts.java
+++ Ghosts.java
@@ -1,18 +1,21 @@
 import javax.swing.ImageIcon;
-
 import jdk.internal.vm.compiler.word.Pointer;
 import jdk.tools.jmod.Main;
 
-public class Ghosts{
-    
 
-    public ghostOptics(){
-        Image green, blue, yellow, red;
-        
-      //green = new ImageIcon("*/Bilder/Ghost.gif").getImage();      -> Farben müssen noch ertsllt werden
-      //blue = new ImageIcon("*/Bilder/Ghost.gif").getImage();
-        yellow = new ImageIcon("*/Bilder/Ghost.gif").getImage();
-      //red = new ImageIcon("*/Bilder/Ghost.gif").getImage();
+public class Ghosts{
+
+    boolean playing; //muss aus Main class bestimmt werden
+    String direction; //Speichert die Richtung, in die sich der Geist bewegt
+    Image green, yellow, red;  //Pacman hat 4 Bilder für jede mögliche Richtung
+    int numberGhosts = 3; //Jetzt statisch, muss wenn varibel aus der main class bestimmt werden
+    int position[][] = new int[2][numberGhosts]; //Position der Geister
+    
+    public void optics(){
+        //Farben für die 3 unterschiedlichen Geister     
+        green = new ImageIcon("*/Bilder/greenGhost.gif").getImage();      // Farben müssen noch ertsllt werden
+        yellow = new ImageIcon("*/Bilder/yellowGhost.gif").getImage();
+        red = new ImageIcon("*/Bilder/redGhost.gif").getImage();
     }
 
     int followrange = Main.followrange;  //-> muss in main noch deklariert werden
@@ -27,14 +30,7 @@ public class Ghosts{
 
         }
     }
-    public void blueGhostMovement(){
-        int ghostRow = 1;
-        int ghostColumn = 1;
-        if(calculateDistance(ghostRow, ghostColumn) < followrange ){
-            huntPacman();
-        }
-
-    }
+    
     public void yellowGhostMovement(){
         int ghostRow = 1;
         int ghostColumn = 1;
@@ -62,7 +58,7 @@ public class Ghosts{
     }
 
     public int calculateDistance(int ghostRow, int ghostColumn){
-        Vector2Int pacmanRow = Pacman.pacmanposition.get
+        Vector2Int pacmanposition = Pacman.position;
 
 
         return Math.abs(pacmanRow - ghostRow) + Math.abs(pacmanColumn - ghostColumn);
--- Pacman.java
+++ Pacman.java
@@ -1,4 +1,6 @@
+import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import javax.swing.ImageIcon;
 
@@ -10,14 +12,16 @@ public class Pacman implements ActionListener{
     boolean playing; //muss aus Main class bestimmt werden
     String direction; //Speichert die Richtung, in die sich der Pacman bewegt
     Image imgup, imgdown, imgleft, imgright;  //Pacman hat 4 Bilder für jede mögliche Richtung
-    Vector2Int position = new Vector2Int(); //Position des Pacman
+    Vector2Int position = new Vector2Int(x, y); //Position des Pacman
 
-    public void pacmanOptics(){
+
+    public void optics(){
         //Hier werden die 4 Bilder geladen
          imgup = new ImageIcon("*/Bilder/PacUp.gif").getImage();
          imgdown = new ImageIcon("*/Bilder/PacDown.gif").getImage();
          imgleft = new ImageIcon("*/Bilder/PacLeft.gif").getImage();
          imgright = new ImageIcon("*/Bilder/PacRight.gif").getImage();
+         
     }
 
     public class Movement extends KeyAdapter{
@@ -50,12 +54,23 @@ public class Pacman implements ActionListener{
         }else if(direction.equals("up")){
             g2d.drawImage(imgup, position.x, position.y + 1, this);
         }else if(direction.equals("left")){
-            g2.drawImage(imgleft, position.x -1, position.y, this);
+            g2d.drawImage(imgleft, position.x -1, position.y, this);
         }else if(direction.equals("right")){
             g2d.drawImage(imgright, position.x + 1, position.y, this);
         }
-
+        int x = position.x;
+        int y = position.y;
     }
 
+    public void lives(Graphics2D g2d){
+
+        Image heart = new ImageIcon("*/Bilder/Heart.png").getImage();
+        int lives; //aus MAin bestimmen
+    
+        for(int i = 0; i < lives; i++){
+            g2d.drawImage(heart, heart.getWidth(observer)*i + 20 , 300 - heart.getHeight(observer), this);
+    
+        }
+    }
 
 }
