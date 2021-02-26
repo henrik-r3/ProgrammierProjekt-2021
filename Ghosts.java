acd30128ffeb09e14656f24d07e1f72dcce4d0e7
henrik-r3 <79112168+henrik-r3@users.noreply.github.com>
2021-02-26 20:24:32 +0100

    Scoreboard hinzufügen

--- Ghosts.java
+++ Ghosts.java
@@ -1,3 +1,5 @@
+import java.awt.Graphics2D;
+
 import javax.swing.ImageIcon;
 import jdk.internal.vm.compiler.word.Pointer;
 import jdk.tools.jmod.Main;
@@ -5,26 +7,27 @@ import jdk.tools.jmod.Main;
 
 public class Ghosts{
 
-    boolean playing; //muss aus Main class bestimmt werden
+    boolean playing; //muss aus Main class bestimmt werden 
     String direction; //Speichert die Richtung, in die sich der Geist bewegt
     Image green, yellow, red;  //Pacman hat 4 Bilder für jede mögliche Richtung
     int numberGhosts = 3; //Jetzt statisch, muss wenn varibel aus der main class bestimmt werden
-    int position[][] = new int[2][numberGhosts]; //Position der Geister
+    Vector2Int positiongreen = new Vector2Int(x, y);
+    Vector2Int positionyellow = new Vector2Int(x, y); //Position der einzelnen Geister
+    Vector2Int positionred = new Vector2Int(x, y);
+    int followrange = Main.followrange;  //-> muss in main noch deklariert werden
     
     public void optics(){
         //Farben für die 3 unterschiedlichen Geister     
-        green = new ImageIcon("*/Bilder/greenGhost.gif").getImage();      // Farben müssen noch ertsllt werden
+        green = new ImageIcon("*/Bilder/greenGhost.gif").getImage();      // Farben müssen noch erstellt werden
         yellow = new ImageIcon("*/Bilder/yellowGhost.gif").getImage();
         red = new ImageIcon("*/Bilder/redGhost.gif").getImage();
     }
 
-    int followrange = Main.followrange;  //-> muss in main noch deklariert werden
-
     public void greenGhostMovement(){
-        int ghostRow = 1;
-        int ghostColumn = 1;
+        positiongreen.x = 0;
+        positiongreen.y = 0;
 
-        if(calculateDistance(ghostRow, ghostColumn) < followrange ){
+        if(calculateDistance(positiongreen.x, positiongreen.y) < followrange ){
             huntPacman();
         } else {
 
@@ -42,26 +45,32 @@ public class Ghosts{
     public void redGhostMovement(){
         int ghostRow = 1;
         int ghostColumn = 1;
-        if(calculateDistance(ghostRow, ghostColumn) < followrange ){
+        if(calculateDistance(ghostRow, ghostColumn) < 2 ){
             huntPacman();
         } else {
             int counter = 0;
             
-
-
         }
 
     }
 
-    public void huntPacman(){
-        
+    public void drawGhosts(Graphics2D g2d){
+
+        g2d.drawImage(green,positiongreen.x, positiongreen.y);
+        g2d.drawImage(yellow, positionyellow.x, positionyellow.y);
+        g2d.drawImage(red, positionred.x, positionrerd.y);
+
     }
 
-    public int calculateDistance(int ghostRow, int ghostColumn){
-        Vector2Int pacmanposition = Pacman.position;
+    public void huntPacman(){
+    
+            //Algorithmus zur Berechnung des Wegs zum Pacman  !!!! -> nur für den Roten, der dann die ganze Zeit folgt??? der Rest maht einfach sein Ding?
+    }
 
+    public int calculateDistance(int row, int column){
+        Vector2Int pacmanposition = new Vector2Int(Pacman.x, Pacman.y);
 
-        return Math.abs(pacmanRow - ghostRow) + Math.abs(pacmanColumn - ghostColumn);
+        return Math.abs(packmanposition.x - row) + Math.abs(pacmanposition.y - column);
          
     }
 
