3fc7283a2fbbb1743570de61184aa58edc40d24a
henrik-r3 <79112168+henrik-r3@users.noreply.github.com>
2021-02-27 22:18:04 +0100

    Update Score.java

--- Score.java
+++ Score.java
@@ -1,5 +1,29 @@
 public class Score{
 
-
+    private int scoreval;
     
+    public Userscore(){
+        scoreval = 0;
+    }
+
+    public void eatsFood(){
+
+        scoreval++;
+    }
+
+    public void eatsPowerBerry(){
+
+        scoreval+= 10;
+    }
+
+    public void eatsGhost(){
+
+        scoreval += 100;
+    }
+
+    public int getScore(){
+
+        return scoreval;
+    }
+
 }
