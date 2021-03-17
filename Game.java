7e7f85b73c0b7ce2f0d23f4b2977544fbaa00e6f
Sytoplis <64976325+Sytoplis@users.noreply.github.com>
2021-03-17 14:11:43 +0100

    s

--- Game.java
+++ Game.java
@@ -8,7 +8,7 @@ import javax.swing.JFrame;
 //Handles GameObjects, causes Updates and Runs the Game in general (e.g. Pausing, etc.)
 public class Game{
     public static Game instance;
-    public int difficulty = 1;
+    public int difficulty = 2;
 
     JFrame frame;
     private Graphics g;
@@ -29,11 +29,7 @@ public class Game{
 
         gameObjects = new ArrayList<GameObject>();//add initial GameObjects
 
-        new Map(new Vector2Int(15, 15));
-        tileSize = new Vector2Int(frame.getWidth() / Map.instance.size.x, frame.getHeight() / Map.instance.size.y);
-        frame.setSize(tileSize.x*Map.instance.size.x, tileSize.y*Map.instance.size.y+30);//+50 für die titelleiste
-
-        new Score();//creates a score object
+        
 
         CreateGameFromDifficulty();
 
@@ -48,23 +44,36 @@ public class Game{
     }
 
     public void CreateGameFromDifficulty(){
+        new Map(new Vector2Int(difficulty*5+10, difficulty*5+10));
+        tileSize = new Vector2Int(frame.getWidth() / Map.instance.size.x, frame.getHeight() / Map.instance.size.y);
+        frame.setSize(tileSize.x*Map.instance.size.x, tileSize.y*Map.instance.size.y+30);//+50 für die titelleiste
 
-        Map.instance.generateMap(rnd, 20);      
+        new Score();//creates a score object
+
+        Map.instance.generateMap(rnd, Map.instance.size.x+Map.instance.size.y);     
 
         //add Gameobjects
         Vector2Int startPos = Map.instance.getRandomPos();
         gameObjects.add( new Pacman(startPos.x, startPos.y, 3) );
 
-        int GhostCount = difficulty * 2;
+        int GhostCount = (int)(difficulty * 1.2);
+        int PowerberryCount = (int)(difficulty * 1.4);
+
+        //create ghosts
         String[] ghostColors = {"green", "pink", "red", "yellow"};
         for(int i = 0; i < GhostCount; i++)
         {
             startPos = Map.instance.getRandomPos();
             gameObjects.add( new Ghosts(startPos.x, startPos.y, 5, ghostColors[rnd.nextInt(ghostColors.length)]));
         }
+
+        //create powerberrys
+        for(int i = 0; i < PowerberryCount; i++)
+        {
+            startPos = Map.instance.getRandomPos();
+            gameObjects.add( new Powerberry(startPos.x, startPos.y));
+        }
         
-        startPos = Map.instance.getRandomPos();
-        gameObjects.add( new Powerberry(startPos.x, startPos.y));
     }
 
 
